b15ea40c37e6ef9afb8752430c2ad7bf8b5d6dfb
Daniel Vaknin <danielvaknin10@gmail.com>
2021-04-18 21:33:34 +0300

    Enable CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes import *
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.register_blueprint(recommendations, url_prefix='/recommendations')
 
